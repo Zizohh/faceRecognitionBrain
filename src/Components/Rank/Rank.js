6df951914011e8de5d0f5558aad54d1e67947ed7
Zizohh <jeremiahhussaini2892@gmail.com>
2020-07-28 05:01:41 +0100

    Smart Brain Face-detector

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Rank = () =>{
+	return (
+		<div className='tc'>
+			<div className='white f3'>
+			{'Zizoh, your current rank is ...'}	
+			</div>
+			<div className='white f1'>
+			{'#10'}	
+			</div>
+			
+		</div>
+	);
+}
+
+export default Rank;
